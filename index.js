0d8e5518211e4254aa9d50ac88af5744ef1feabb
HuhnDaniel <danielphuhn@gmail.com>
2020-06-02 14:50:38 -0500

    added empty functions for each action

--- index.js
+++ index.js
@@ -33,25 +33,71 @@ function promptUser() {
 }
 
 async function init() {
-	const { action } = await promptUser();
+	try {
+		const { action } = await promptUser();
 
-	switch (action) {
-		case "Add department":
-			break;
-		case "Add role":
-			break;
-		case "Add employee":
-			break;
-		case "View department":
-			break;
-		case "View role":
-			break;
-		case "View employee":
-			break;
-		case "Update employee role":
-			break;
-		case "Exit program":
-			connection.end();
-			break;
+		switch (action) {
+			case "Add department":
+				addDepartment();
+				break;
+
+			case "Add role":
+				addRole();
+				break;
+
+			case "Add employee":
+				addEmployee();
+				break;
+
+			case "View department":
+				viewDepartment();
+				break;
+
+			case "View role":
+				viewRole();
+				break;
+
+			case "View employee":
+				viewEmployee();
+				break;
+
+			case "Update employee role":
+				updateEmployeeRole();
+				break;
+
+			case "Exit program":
+				connection.end();
+				break;
+		}
+	} catch (err) {
+		console.log(err);
 	}
+}
+
+function addDepartment() {
+	init();
+}
+
+function addRole() {
+	init();
+}
+
+function addEmployee() {
+	init();
+}
+
+function viewDepartment() {
+	init();
+}
+
+function viewRole() {
+	init();
+}
+
+function viewEmployee() {
+	init();
+}
+
+function updateEmployeeRole() {
+	init();
 }
