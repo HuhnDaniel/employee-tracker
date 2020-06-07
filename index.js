030db201107ec33b351d16f69573d181830d9bce
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-06-07 14:47:18 -0500

    moved connection to own file to clean up index

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-var mysql = require(`mysql`);
+var connection = require(`./assets/connection`);
 var { prompt } = require(`inquirer`);
 
-var connection = mysql.createConnection({
-	host: `localhost`,
-	port: 3306,
-	user: `root`,
-	password: `password`,
-	database: `company_db`
-});
-
-connection.connect((err) => {
-	if (err) throw err;
-	init();
-});
+init();
 
 // Main function that takes user input and directs them accordingly
 function init() {
